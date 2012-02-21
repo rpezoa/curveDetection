2e077fe460cc946f5774944fb7677e461b27da82
Raquel Pezoa <rpezoarivera@gmail.com>
2012-02-21 18:48:36 -0300

    mofifying generateFullTable, not ready yet

--- code/generateFullTable.m
+++ code/generateFullTable.m
@@ -1,26 +1,48 @@
-function [alTable,M,Angulos,magx,magy,newIm] = generateFullTable(nameImage)
+function [alTable,M,Mag1,Angulos,magx,magy,newIm,Px_5,Py_5,ab5,newCurv] = generateFullTable(nameImage)
 
 newIm=imread(nameImage);
 newIm=rgb2gray(newIm);
 
 [Curvature,Mag1,M,Angulos,Next,magx,magy]=CurvConnect(newIm);
 
-
 [alTable]=runAlongCurve(newIm,250,0,1);%(inputimage,t,p,isGray)
-
-
 
 [rows]=size(alTable,1);
 
-for i=2:rows-1
+N=10;
+%absisa5=generaCadaNpuntos(alTable(:,1)',N)Px_5=generaCadaNpuntos(alTable(:,1)',N);;
+Px_5=generaCadaNpuntos(alTable(:,1)',N)';
+Py_5=generaCadaNpuntos(alTable(:,2)',N)';
+ab5=generaCadaNpuntos(alTable(:,3)',N)';
+
+[newRows]=size(Px_5,1);
+newCurv=zeros(newRows,1);
+for i=2:newRows-1
+  
+    Px5=Px_5(i,1); %coord x de pixel actual
+    Py5=Py_5(i,1); %coord y de pixel actual
+    Px5_pre=double(Px_5(i-1,1)); %coord x de pixel anterior
+    Py5_pre=double(Py_5(i-1,1)); %coord y de pixel anterior
+    Px5_post=double(Px_5(i+1,1)); %coord x de pixel posterior
+    Py5_post=double(Py_5(i+1,1)); %coord y de pixel posterior
+    c=sqrt((Px5_post - 2* Px5 + Px5_pre)^2 +  (Py5_post - 2* Py5 + Py5_pre)^2);
+    newCurv(i,1)=c;
     
+
+end
+
+
+for i=2:rows-1
+  
     Px=alTable(i,1); %coord x de pixel actual
     Py=alTable(i,2); %coord y de pixel actual
-    Px_pre=alTable(i-1,1); %coord x de pixel anterior
-    Py_pre=alTable(i-1,2); %coord y de pixel anterior
-    Px_post=alTable(i+1,1); %coord x de pixel posterior
-    Py_post=alTable(i+1,2); %coord y de pixel posterior
+    Px_pre=double(alTable(i-1,1)); %coord x de pixel anterior
+    Py_pre=double(alTable(i-1,2)); %coord y de pixel anterior
+    Px_post=double(alTable(i+1,1)); %coord x de pixel posterior
+    Py_post=double(alTable(i+1,2)); %coord y de pixel posterior
     curv=sqrt((Px_post - 2* Px + Px_pre)^2 +  (Py_post - 2* Py + Py_pre)^2);
+    
+    
     alTable(i,5)=curv; % curvatura
     alTable(i,6)=M(Px,Py); % magnitud gradiente
     alTable(i,7)=Angulos(Px,Py); % direccion gradiente
@@ -47,6 +69,7 @@ for i=2:rows-1
     alTable(i,10)=magx(Px, Py);
    alTable(i,11)=magy(Px,Py);
    alTable(i,12)=Curvature(Px,Py);
+   alTable(i,13)=Mag1(Px,Py);
     
 end
  
@@ -62,21 +85,34 @@ xlabel('s (arc length)');
 ylabel('y(s)');
 
 
-%figure;
-%plot(alTable(1:n,3),alTable(1:n,6), '*');
+figure;
+subplot(3,1,1); plot(alTable(:,3),alTable(:,5), '*');
+title('Curvature: "sqrt((Px_{post} - 2* Px + Px_{pre})^2 +  (Py_{post} - 2* Py + Py_{pre})^2)"');
+xlabel('s (arc length)');
+ylabel('curvature(s)');
 
-%figure;
-%plot(alTable(1:n,3),alTable(1:n,7), '*');
+subplot(3,1,2);plot(alTable(:,3),alTable(:,8), '*');
+title('Diferencia de Angulos');
+xlabel('s (arc length)');
+ylabel('Dif. angles');
+
+subplot(3,1,3);plot(alTable(:,3),alTable(:,9));
+title('Diferencia de Gradiente');
+xlabel('s (arc length)');
+ylabel('Dif. grad.');
+
 
 %figure; % diferencia de angulos
 %plot(alTable(:,3),alTable(:,8), 'g');
 %hold on;
-%figure; % diferencia de gradiente
-%plot(alTable(:,3),alTable(:,9), 'r');
+
 %hold on;
 
-%figure; % curvatura
-%plot(alTable(:,3),alTable(:,12), 'm');
+figure; % curvatura
+plot(alTable(:,3),alTable(:,12), 'm');
+title('Curvature');
+xlabel('s (arc length)');
+ylabel('curvature(s)');
 
 
 
