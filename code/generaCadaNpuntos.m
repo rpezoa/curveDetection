eae5d6046896c7955e9b3040fe6b0c0119e52a5c
Raquel Pezoa <rpezoarivera@gmail.com>
2012-02-22 17:58:18 -0300

    adding comments to generaCadaNpuntos.m

--- code/generaCadaNpuntos.m
+++ code/generaCadaNpuntos.m
@@ -1,12 +1,14 @@
 function [newPuntos] = generaCadaNpuntos(puntos,N)
+% puntos: vector de la forma [x1 x2 ... xn]
+% N: the step to select elements of the vector puntos
 
-d = size(puntos,2)
+d = length(puntos);
 count=0;
-newPuntos=zeros(1,round(d/N));
+newPuntos=zeros(1,round(d/N)); % new vector
 j=2;
 newPuntos(1)=puntos(1);
 for i=2:d
-    if mod(i,N) == 0
+    if mod(i,N) == 0 % if the element puntos(i) is a multiple of N, it is matained.
     newPuntos(j) = puntos(i);
     j=j+1;
     end
