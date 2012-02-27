57656c62db1b1f3530f77d8ed1b852ab05c3571f
Raquel Pezoa <rpezoarivera@gmail.com>
2012-02-27 17:56:19 -0300

    to calculate the IDFT

---
+++ code/nudft/myInvDFT.m
@@ -0,0 +1,26 @@
+function [X] = myInvDFT(X,t)
+% x: input signal
+
+disp('Calculating DFT ::: ');
+
+N = length(X);
+
+x=zeros(1,N);
+
+% the k index indicates the frequency and the i index indicates the
+% ith-sample
+if t == 0    
+    for k=1:N
+        for i=1:N
+            x(k)= X(i)*exp(1j*2*pi*(k-1)*(i-1)/N) + x(k);
+        end        
+        x(k)=x(k)/N;
+    end
+else
+    for n=1:N % (indice para el vector de la señal x)
+        for m=1:N %(indice para el vector de la frecuencia)
+            x(n) = X(m)*exp(1j*2*pi*(t(m))*(m-1)/N) + x(n);
+        end
+    end
+end
+    
