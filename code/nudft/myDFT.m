ba85fb560fabb89ad571c2259cff3edce5588ae6
Raquel Pezoa <rpezoarivera@gmail.com>
2012-02-28 15:35:56 -0300

    small modif

--- code/nudft/myDFT.m
+++ code/nudft/myDFT.m
@@ -12,7 +12,6 @@ X=zeros(1,N);
 % the k index indicates the frequency and the i index indicates the
 % ith-sample
 if t == 0
-    
     for k=1:N
         for i=1:N
             X(k)= x(i)* exp(-1j*2*pi*(k-1)*(i-1)/N) + X(k);
@@ -28,3 +27,10 @@ else
     end
 end
     
+omega_N=zeros(1,N);
+
+for k=1:N
+    for n=1:N
+    omega_N(n)=exp(-1j*2*pi*k*(n-1)/N);
+    end
+end
